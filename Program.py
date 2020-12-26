c3210accb7fb7bf2ef3044d4ece1bde24fbc7cca
TOGNETTI Yohann <56682531+Idrash@users.noreply.github.com>
2020-12-26 22:52:16 +0100

    Ajout d'un generateur de randomState
    Ajout d'un generateur de random state pour eviter de faire plusieur execution differente pour les graphique

--- Program.py
+++ Program.py
@@ -2,6 +2,9 @@ import pandas as pd
 import string
 import xml.etree.ElementTree as et
 import numpy as np
+import random 
+import json
+from os import path
 
 #sklearn
 from sklearn.model_selection import train_test_split
@@ -23,6 +26,8 @@ from nltk.stem import WordNetLemmatizer
 
 #graphique
 import matplotlib.pyplot as plt
+
+FILE_RANDOM_STATE = "randomState.json"
 
 PONCTUATION = set(string.punctuation)
 STOP_WORDS = stopwords.words("english")
@@ -193,7 +198,7 @@ def CreateClassifieur(df):
 
         Classifieur[category] = bestClf
         prediction = bestClf.predict(x_resample)
-        report = classification_report(y_resample,prediction)
+        report = classification_report(y_resample,prediction,zero_division=1)
         print(report)
 
 
@@ -204,7 +209,7 @@ def CreateClassifieur(df):
     ])
     Classifieur['polarity'] = LogReg_pipeline.fit(x_train, y_train['polarity'])
     prediction = Classifieur['polarity'].predict(x_train)
-    print(classification_report(y_train['polarity'],prediction))
+    print(classification_report(y_train['polarity'],prediction,zero_division=1))
 
     return Classifieur,vectorizer,ClassifyCategory
 
@@ -263,13 +268,13 @@ def predictTestData(Classifieur,vectorizer,ClassifyCategory,dfTest):
         print("Pour la categorie",category,":")
         prediction = Classifieur[category].predict(x_test)
 
-        print(classification_report(y_test[category],prediction))
+        print(classification_report(y_test[category],prediction,zero_division=1))
         scores.append(np.mean(precision_score(y_test[category],prediction,average=None)))
         names.append(category)
 
     #polarity classification
     predictionPol = Classifieur['polarity'].predict(x_test)
-    print(classification_report(y_test['polarity'],predictionPol))
+    print(classification_report(y_test['polarity'],predictionPol,zero_division=1))
 
     plt.bar(names, scores)
     plt.xticks(rotation="vertical")
@@ -280,39 +285,199 @@ def predictTestData(Classifieur,vectorizer,ClassifyCategory,dfTest):
 
 
 
+def generateRandomStateDictionary(df,categoryToGenerate,maxExec=100,maxRandomState=5000,
+    randomState=dict(),writeInFile=True,fileName=FILE_RANDOM_STATE ,upgradeMode=True):
 
-df = getDFFromXML("train_data.xml")
-print(df)
+    if(maxExec>=maxRandomState/10):
+        raise Exception("generateRandomStateDictionary: value not accepted -> Max exec >= maxRandomeState/10")
 
-df['text'] = df.text.apply(lambda text : preProcessing(text))
-print(df)
+    df = df.drop(labels = ['id'], axis=1) #No Use
 
-dfTest = getDFFromXML("test_data.xml")
-print(dfTest)
-dfTest['text'] = dfTest.text.apply(lambda text : preProcessing(text))
-print(dfTest)
+    vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word', ngram_range=(1,8), norm='l2')
+    matrice = vectorizer.fit_transform(df.text)
 
-print("\n\n#################################################################################################\n\n")
-#classification(df)
+    #voir pour avoir le fichier de test
+    x_train = vectorizer.transform(df.text)
+    y_train = df.drop(labels = ['text'], axis=1)
+    progress = 0
+    for category in categoryToGenerate:
+        
+        print("Generate random state progress : ",progress/len(categoryToGenerate)*100,"%")
 
-Classifieur,vectorizer,ClassifyCategory = CreateClassifieur(df)
+        nb_class = getNumberOfClass(y_train[category])
+        # il n'y a pas plusieur categorie on ne peut pas trouver un bon randome state
+        if(nb_class <= 1):
+            randomState[category]=-1
+            continue
 
-predictTestData(Classifieur,vectorizer,ClassifyCategory,dfTest)
+        x_resample, y_resample = x_train,y_train[category]
 
-print("\n\n#################################################################################################\n\n")
-ClassifyString(Classifieur,vectorizer,ClassifyCategory,"W7 Pro with W8 pro upgrade is nice, but it frequently freezes for a few seconds here and there.")
-print("\n\n#################################################################################################\n\n")
-ClassifyString(Classifieur,vectorizer,ClassifyCategory,"It took some getting used to on the Widows 8.1, after being an XP user for many years.")
-print("\n\n#################################################################################################\n\n")
-ClassifyString(Classifieur,vectorizer,ClassifyCategory,"Other than a hate of windows 8, she just loves it.")
-print("\n\n#################################################################################################\n\n")
-ClassifyString(Classifieur,vectorizer,ClassifyCategory,"So with this new order, all the upgrades and windows 8 really caught my eye and I got this for the long haul.")
-print("\n\n#################################################################################################\n\n")
-ClassifyString(Classifieur,vectorizer,ClassifyCategory,"The Windows 8.1 used to boot in just 2s and that was freaking awesome.")
-print("\n\n#################################################################################################\n\n")
-ClassifyString(Classifieur,vectorizer,ClassifyCategory,"I liked the fact that it came with Windows 7, but also included Windows 8 to install later if I need it.")
-print("\n\n#################################################################################################\n\n")
-ClassifyString(Classifieur,vectorizer,ClassifyCategory,"The 8.1 windows is a major disappointment.")
+        bestScore = 0
+        bestRandomState = 0
+        alreadyTestRandom = dict()
+        nbExec = 0
+        for nbExec in range(maxExec):
+            #premiere execution on recuprer un element deja existant si possible (si l'on veut ameillioré un resultat deja existant)
+            if(nbExec==0 and upgradeMode and category in randomState and randomState[category]!=-1):
+                currentRandom = randomState[category]
+            else:
+                #generation d'un random non utilisé
+                currentRandom = random.randint(1,maxRandomState)
+                while (currentRandom in alreadyTestRandom):
+                    currentRandom = random.randint(1,maxRandomState)
+
+            # Using pipeline for applying logistic regression and one vs rest classifier
+            LogReg_pipeline = Pipeline([
+                    ('clf', OneVsRestClassifier(LogisticRegression(solver='sag', class_weight='balanced',random_state=currentRandom, max_iter=1000, n_jobs=-1), n_jobs=-1)),
+                ])
+
+            # Training logistic regression model on train data
+            currentClf = LogReg_pipeline.fit(x_resample, y_resample)
+    
+            prediction = currentClf.predict(x_resample)
+
+            #si incorrect on refait le train
+            accuracy = accuracy_score(y_resample, prediction)
+
+            if(accuracy>bestScore):
+                bestScore=accuracy
+                bestRandomState = currentRandom
+                #Best score
+                if(accuracy==1):
+                    break
+
+
+
+        randomState[category]=bestRandomState
+        progress+=1
+
+    print(randomState)
+
+    #write on file
+    with open(fileName, 'w') as file:
+     file.write(json.dumps(randomState)) # use `json.loads` to do the reverse
+
+    return randomState
+
+def CreateClassifieurWithRandomState(df,randomStateDict):
+    df = df.drop(labels = ['id'], axis=1) #No Use
+
+    vectorizer = TfidfVectorizer(strip_accents='unicode', analyzer='word', ngram_range=(1,8), norm='l2')
+    matrice = vectorizer.fit_transform(df.text)
+
+    #voir pour avoir le fichier de test
+    x_train = vectorizer.transform(df.text)
+    y_train = df.drop(labels = ['text'], axis=1)
+    
+    Classifieur = dict()
+
+    ClassifyCategory = []
+   
+    for category in ALL_CATEGORIES:
+
+        
+        #INCREASE POPULATION 
+        #ADA = ADASYN(sampling_strategy='auto',n_neighbors = 4)
+    
+        nb_class = getNumberOfClass(y_train[category])
+        if(nb_class > 1): #can't resample if there is one class.
+            #x_resample, y_resample =  ADA.fit_resample(x_train,y_train[category])
+            ClassifyCategory.append(category)
+        else :
+            continue
+        
+        x_resample, y_resample = x_train,y_train[category]
+
+        print('**Processing {} comments...**'.format(category))
+        # Using pipeline for applying logistic regression and one vs rest classifier
+        LogReg_pipeline = Pipeline([
+                ('clf', OneVsRestClassifier(LogisticRegression(solver='sag', class_weight='balanced',random_state=randomStateDict[category] ,max_iter=1000, n_jobs=-1), n_jobs=-1)),
+            ])
+
+        # Training logistic regression model on train data
+        Clf = LogReg_pipeline.fit(x_resample, y_resample)
+
+
+        Classifieur[category] = Clf
+        prediction = Clf.predict(x_resample)
+        report = classification_report(y_resample,prediction,zero_division=1)
+        print(report)
+
+
+    #POLARITY
+    print('**Processing polarity...**')
+    LogReg_pipeline = Pipeline([
+        ('clf', OneVsRestClassifier(LogisticRegression(solver='sag', class_weight='balanced',random_state=randomStateDict["polarity"] ,max_iter=1000 ,n_jobs=-1), n_jobs=-1)),
+    ])
+    Classifieur['polarity'] = LogReg_pipeline.fit(x_train, y_train['polarity'])
+    prediction = Classifieur['polarity'].predict(x_train)
+    print(classification_report(y_train['polarity'],prediction,zero_division=1))
+
+    return Classifieur,vectorizer,ClassifyCategory
+
+
+def computeRandomStateArray(df):
+    if(path.exists(FILE_RANDOM_STATE)):
+        f = open(FILE_RANDOM_STATE, "r")
+        try:
+            randomStateDict=json.load(f)
+            print(randomStateDict)
+        except:
+            randomStateDict=dict()
+    else:
+        randomStateDict=dict()
+
+    #permet de compute pour tout les categorie (stock dans un fichier donc en commentaire mtn)
+    CatRandomState = ALL_CATEGORIES[:]
+    CatRandomState.append("polarity")
+    generateRandomStateDictionary(df,CatRandomState,randomState=randomStateDict,maxExec=100)
+
+def main():
+
+    df = getDFFromXML("train_data.xml")
+    print(df)
+
+    df['text'] = df.text.apply(lambda text : preProcessing(text))
+    print(df)
+    print("\n\n#################################################################################################\n\n")
+    #classification(df)
+
+    dfTest = getDFFromXML("test_data.xml")
+    print(dfTest)
+    dfTest['text'] = dfTest.text.apply(lambda text : preProcessing(text))
+    print(dfTest)
+
+    print("\n\n#################################################################################################\n\n")
+    #classification(df)
+    # Classifieur,vectorizer,ClassifyCategory = CreateClassifieur(df)
+    #predictTestData(Classifieur,vectorizer,ClassifyCategory,dfTest)
+
+    if(path.exists(FILE_RANDOM_STATE)):
+        f = open(FILE_RANDOM_STATE, "r")
+        randomStateDict=json.load(f)
+
+    Classifieur,vectorizer,ClassifyCategory = CreateClassifieurWithRandomState(df,randomStateDict)
+    predictTestData(Classifieur,vectorizer,ClassifyCategory,dfTest)
+
+    # print("\n\n#################################################################################################\n\n")
+    # ClassifyString(Classifieur,vectorizer,ClassifyCategory,"W7 Pro with W8 pro upgrade is nice, but it frequently freezes for a few seconds here and there.")
+    # print("\n\n#################################################################################################\n\n")
+    # ClassifyString(Classifieur,vectorizer,ClassifyCategory,"It took some getting used to on the Widows 8.1, after being an XP user for many years.")
+    # print("\n\n#################################################################################################\n\n")
+    # ClassifyString(Classifieur,vectorizer,ClassifyCategory,"Other than a hate of windows 8, she just loves it.")
+    # print("\n\n#################################################################################################\n\n")
+    # ClassifyString(Classifieur,vectorizer,ClassifyCategory,"So with this new order, all the upgrades and windows 8 really caught my eye and I got this for the long haul.")
+    # print("\n\n#################################################################################################\n\n")
+    # ClassifyString(Classifieur,vectorizer,ClassifyCategory,"The Windows 8.1 used to boot in just 2s and that was freaking awesome.")
+    # print("\n\n#################################################################################################\n\n")
+    # ClassifyString(Classifieur,vectorizer,ClassifyCategory,"I liked the fact that it came with Windows 7, but also included Windows 8 to install later if I need it.")
+    # print("\n\n#################################################################################################\n\n")
+    # ClassifyString(Classifieur,vectorizer,ClassifyCategory,"The 8.1 windows is a major disappointment.")
+
+
+
+if __name__=="__main__":
+    main()
 
 
 
